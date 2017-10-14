ba70b26e6108bad406dc4f656690ae87204d7aae
G <igauravsehrawat@gmail.com>
2017-10-14 18:40:09 +0530

    Worked on routing.

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <h3>Not Found!!!</h3>
+    )
+}
+
+export default NotFound;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,9 +2,24 @@
 // bring up the Hello in a component name
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss} from 'react-router';
 
 import './css/style.css';
+
 import App from './components/App';
+import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker}/>
+                <Match pattern="/store/:storeId" component={App}/>
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+render(<Root/>, document.querySelector('#main'));
