6ec158f8870bdb9f2f18fc29dea881372abc173c
G <igauravsehrawat@gmail.com>
2017-10-14 16:39:36 +0530

    Learned HTML and JSX.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello</p>;
+        return (
+            <form className="store-selector">
+                { /* this is a comment */}
+                <input type="text" placeholder="Enter the store name" required/>
+                <button type="submit">Click here -></button>
+            </form>
+        )
     }
 }
 
