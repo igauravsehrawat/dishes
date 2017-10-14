071216e63a1efc21e5d0a1335fdad0d5618ccd1b
G <igauravsehrawat@gmail.com>
2017-10-14 16:44:09 +0530

    Adding styles in react.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,8 @@
 // bring up the Hello in a component name
 import React from 'react';
 import { render } from 'react-dom';
+
+import './css/style.css';
 import StorePicker from './components/StorePicker';
 
 console.log(StorePicker);
