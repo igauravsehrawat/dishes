b2b498cc81eb16c52c5bd679c1458536b8ce8daf
G <igauravsehrawat@gmail.com>
2017-10-14 16:07:36 +0530

    Implemented first component as StorePicker.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
 // let's go!
+// bring up the Hello in a component name
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+console.log(StorePicker);
+
+render(<StorePicker/>, document.querySelector('#main'));
