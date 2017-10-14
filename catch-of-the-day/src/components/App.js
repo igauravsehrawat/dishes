b0225df7d1a541556f99a8183cee7f4f0110da2f
G <igauravsehrawat@gmail.com>
2017-10-14 18:00:15 +0530

    Using props in the react.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Fresh SeaFood Market"/>
                 </div>
                 <Order />
                 <Inventory />
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -3,7 +3,15 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-            <p>Header</p>
+            <header className="top">
+                <h1>Catch
+                    <span className="ofThe">
+                        <span className="of">Of</span>
+                        <span className="the">The</span>
+                    </span>
+                    Day</h1>
+                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+            </header>
         )
     }
 }
