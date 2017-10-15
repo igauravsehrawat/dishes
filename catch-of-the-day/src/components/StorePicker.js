4aac6a490583e73972bdac68026f4b62b2b1f4f2
G <igauravsehrawat@gmail.com>
2017-10-15 16:04:52 +0530

    Using helper function for store name.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
@@ -6,7 +7,7 @@ class StorePicker extends React.Component {
             <form className="store-selector">
                 { /* this is a comment */}
                 <h1>Enter a Store</h1>
-                <input type="text" placeholder="Store name" required/>
+                <input type="text" placeholder="Store name" required defaultValue={getFunName()}/>
                 <button type="submit">Click here -></button>
             </form>
         )
