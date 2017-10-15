c8d2da01d8580f09461f3d7790c4e637338b3e40
G <igauravsehrawat@gmail.com>
2017-10-15 16:31:24 +0530

    Passing the value in custom function and then using context for routing. Whew!!

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,24 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+
+    // }
+
+    goToStore(event) {
+        event.preventDefault();
+        const storeValue = this.storeInput.value;
+        this.context.router.transitionTo(`/store/${storeValue}`);
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={ (e) => {this.goToStore(e)}}>
                 { /* this is a comment */}
                 <h1>Enter a Store</h1>
-                <input type="text" placeholder="Store name" required defaultValue={getFunName()}/>
+                <input type="text" placeholder="Store name" required defaultValue={getFunName()}
+                    ref={(input) => {this.storeInput = input}}/>
                 <button type="submit">Click here -></button>
             </form>
         )
@@ -15,3 +27,8 @@ class StorePicker extends React.Component {
 }
 
 export default StorePicker;
+
+// Using context, see note in documentation
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+}
