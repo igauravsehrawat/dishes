f6306be60e1282b240d8c22c729ee5f75f0b121a
G <igauravsehrawat@gmail.com>
2017-12-22 17:25:25 +0530

    Save local changes

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 
 var config = {
-    apiKey: "secret-stuff",
-    authDomain: "secret-stuff",
-    databaseURL: "secret-stuffm"
+    apiKey: "AIzaSyAD5-jDWt1Km42xBNVF3Su8pmwrNd1-3s0",
+    authDomain: "catch-of-the-day-igaurav.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-igaurav.firebaseio.com"
 }
 
 var base = Rebase.createClass(config);
